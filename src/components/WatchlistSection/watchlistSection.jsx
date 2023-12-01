9d294cb4e2c323614167beccc22f800574e67f91
gautamnaik1994 <gautamnaik1994@gmail.com>
2023-12-02 02:19:57 +0530

    added watchlist section

--- src/components/WatchlistSection/watchlistSection.jsx
+++ src/components/WatchlistSection/watchlistSection.jsx
@@ -1,8 +1,13 @@
 function WatchlistSection() {
   return (
-    <section>
-      <h1>Watchlist</h1>
-      <img src="/watchlist.png" alt="watclist" />
+    <section className="watchlist-section">
+      <img src="/eye.svg" alt="bell" />
+      <h1 className="section-title ">Watch what the whales are doing</h1>
+      <p className="section-subtitle">
+        All whales are not equal. Know exactly what the whales impacting YOUR
+        portfolio are doing.
+      </p>
+      <img className="screenshot" src="/watchlist.png" alt="watclist" />
     </section>
   );
 }
